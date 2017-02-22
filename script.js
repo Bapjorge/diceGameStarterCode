32b3154a18b4bec1ba2727a4e52de1f2d787af3c
Jorge Moreno <jorge.a.moreno.personal@gmail.com>
2017-02-22 23:50:34 +0000

    dice

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 $(document).ready(function(){
-  function rolldice(){
-  //Place your code here 
-
-
-  };
   
-});
+  function rolldie(){  
+    
+  var rolldice1 = Math.floor(Math.random(1)*6);
+  var rolldice2 = Math.floor(Math.random(1)*6);
+  
+  var roundednumber = Math.floor(rolldice1);
+  console.log(roundednumber);
+  var url =  "<img src=" + "images/"+ roundednumber +".png" + ">";
+  console.log(url);
+  $("body").append( url );
+
+    
+    
+  };
+
+    
+    
+  $("#roll-dice").click(function(){
+    rolldie();
+  });
+  });
+
